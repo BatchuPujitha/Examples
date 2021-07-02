de00bc52ff3b43f83ed6886d10e760ee37183ba8
BatchuPujitha <85623205+BatchuPujitha@users.noreply.github.com>
2021-07-02 16:54:39 +0530

    Add files via upload

---
+++ Digitalclock.js
@@ -0,0 +1,10 @@
+﻿function display_refreshtime() {
+    var refresh = 1000; 
+    mytime = setTimeout('display_currenttime()', refresh)
+}
+
+function display_currenttime() {
+    var x = new Date()
+    document.getElementById('currenttime').innerHTML = x;
+    display_refreshtime();
+}
---
+++ dropdown.js
@@ -0,0 +1,12 @@
+﻿function Validate(value1) {
+
+    if (value1 == document.getElementById("male").value) {
+        document.getElementById("male").checked = true;
+    }
+    if (value1 == document.getElementById("female").value) {
+        document.getElementById("female").checked = true;
+    }
+    if (value1 == document.getElementById("others").value) {
+        document.getElementById("others").checked = true;
+    }
+}
